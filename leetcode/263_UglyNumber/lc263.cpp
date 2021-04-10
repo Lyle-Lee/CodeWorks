e7b787acf9ad11186ffbcdae150d4d06719521f6
Aoyu Li <51700584+Lyle-Lee@users.noreply.github.com>
2021-04-10 15:15:06 +0900

    Update lc263.cpp

--- leetcode/263_UglyNumber/lc263.cpp
+++ leetcode/263_UglyNumber/lc263.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if (n == 0) return false;
+        if (n <= 0) return false;
         while (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n == 1) {
             if (n == 1) return true;
             if (n % 2 == 0) n = n / 2;
