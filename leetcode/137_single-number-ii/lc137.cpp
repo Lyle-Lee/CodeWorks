563b5ee5394b7fdae8dba546c32564807695aab4
Aoyu Li <51700584+Lyle-Lee@users.noreply.github.com>
2021-04-30 22:44:25 +0900

    Create lc137.cpp

---
+++ leetcode/137_single-number-ii/lc137.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //sort(nums.begin(), nums.end());
+        //int n = nums.size();
+        //int ans = nums[0];
+        //for (int i = 1; i < n; ++i) {
+        //    if (nums[i - 1] < nums[i]) {
+        //        if (i == 1) return ans;
+        //        if (i == n - 1 || nums[i] < nums[i + 1]) {
+        //            ans = nums[i];
+        //            break;
+        //        }
+        //    }
+        //}
+        //return ans;
+        int a = 0, b = 0;
+        for (int num: nums) {
+            b = ~a & (b ^ num);
+            a = ~b & (a ^ num);
+        }
+        return b;
+    }
+};
